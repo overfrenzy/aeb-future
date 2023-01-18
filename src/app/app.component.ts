100cd97a627da5417e8cbe7239787f85454bfb92
overfrenzy <xplo1d387@gmail.com>
2023-01-19 00:14:23 +0900

    ver1.0 frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aeb-future';
-  SelectedExperience: string = 'Нет';
-  changeAnswer(Experience: string) {
-    this.SelectedExperience = Experience;
   }
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DropdownComponent } from './components/dropdown/dropdown.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DropdownComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
